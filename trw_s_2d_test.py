8800242a80b74bd35313a9450e156d1818209f54
Takahiro SUZUKI <takahiro.suzuki.ja@gmail.com>
2016-09-18 14:49:17 +0900

    TRW-S works

--- trw_s_2d_test.py
+++ trw_s_2d_test.py
@@ -8,8 +8,6 @@ import scipy.ndimage
 import numpy as np
 import matplotlib.pyplot as plt
 
-CACHE_PATH = 'cache.npz'
-
 def solve_local(unary, pairwise):
     # local argmin energy. no pairwise term.
     H, W, N_DISP = unary.shape
@@ -40,64 +38,48 @@ def solve_lbp(unary, pairwise):
     return cost.argmin(axis=2)
 
 def solve_trw_s(unary, pairwise):
-    # TRW-S with diagonal updating.
+    # TRW-S min-sum
     H, W, N_DISP = unary.shape
     L, R, U, D = 0, 1, 2, 3
     msg = np.zeros((H, W, N_DISP, 4), np.float32)
     Ebound = 0
-    gamma = 0.25
+    gamma = 1.0 / 4 # 4 edge connected to 1 node
 
     # monotonic chain with an ordering and its reversed version.
-    original_ordering = []
-    for y in range(H):
-        for x in range(W):
-            original_ordering.append((y, x))
+    original_ordering = [(y, x) for y in range(H) for x in range(W)]
     reversed_ordering = reversed(original_ordering)
-    for i in range(4):
+
+    for i in range(2):
         if i % 2 == 0:
             ordering = original_ordering
         else:
             ordering = reversed_ordering
 
         for y, x in ordering:
-            s = unary[y, x, :]
-            # sum_(u,s){M_(u->s)}
-            if i % 2 == 0:
-                if y > 0: s += msg[y - 1, x, :, U]
-                if x > 0: s += msg[y, x - 1, :, L]
-            else:
-                if y + 1 < H: s += msg[y + 1, x, :, D]
-                if x + 1 < W: s += msg[y, x + 1, :, R]
+            # M_(u->s)
+            s = unary[y, x, :] + msg[y, x, :, U] + msg[y, x, :, L] + msg[y, x, :, D] + msg[y, x, :, R]
             delta = np.min(s)
             s -= delta
             Ebound += delta
 
             # M_(s->t) update and normalize
             if i % 2 == 0 and y + 1 < H: # to down
-                v = np.zeros(N_DISP, np.float32) + 1e9
-                for j in range(N_DISP):
-                    v = np.minimum(v, gamma*s[j] - msg[y, x, j, D] + pairwise[j, :])
+                v = np.min((gamma*s - msg[y, x, :, D]) + pairwise, axis=0)
                 delta = np.min(v)
                 msg[y + 1, x, :, U] = v - delta
                 Ebound += delta
             if i % 2 == 0 and x + 1 < W: # to right
-                v = np.zeros(N_DISP, np.float32) + 1e9
-                for j in range(N_DISP):
-                    v = np.minimum(v, gamma*s[j] - msg[y, x, j, R] + pairwise[j, :])
+                v = np.min((gamma*s - msg[y, x, :, R]) + pairwise, axis=0)
                 delta = np.min(v)
                 msg[y, x + 1, :, L] = v - delta
                 Ebound += delta
             if i % 2 == 1 and y > 0: # to up
-                v = np.zeros(N_DISP, np.float32) + 1e9
-                for j in range(N_DISP):
-                    v = np.minimum(v, gamma*s[j] - msg[y, x, j, U] + pairwise[j, :])
+                v = np.min((gamma*s - msg[y, x, :, U]) + pairwise, axis=0)
                 delta = np.min(v)
                 msg[y - 1, x, :, D] = v - delta
                 Ebound += delta
             if i % 2 == 1 and x > 0: # to left
-                v = np.zeros(N_DISP, np.float32) + 1e9
-                for j in range(N_DISP):
-                    v = np.minimum(v, gamma*s[j] - msg[y, x, j, L] + pairwise[j, :])
+                v = np.min((gamma*s - msg[y, x, :, L]) + pairwise, axis=0)
                 delta = np.min(v)
                 msg[y, x - 1, :, R] = v - delta
                 Ebound += delta
@@ -111,14 +93,19 @@ def solve_trw_s(unary, pairwise):
 def trw_s_2d_stereo():
     parser = argparse.ArgumentParser()
     parser.add_argument('--shrink', type=int, default=1)
+    parser.add_argument('--pairwise', default='Potts')
+    parser.add_argument('--coef', type=float, default=1.0)
     parser.add_argument('--method', default='LBP')
+    parser.add_argument('--show-unary', action='store_true')
     args = parser.parse_args()
 
+    # data load
     imL = skimage.io.imread('data/imL.png')
     imR = skimage.io.imread('data/imR.png')
     BASE_DISP = 9
     N_DISP = 9
-    STEP_SIZE = 3
+    STEP_SIZE = 2
+    WINDOW_SIZE = 3
     if args.shrink > 1:
         imL = scipy.ndimage.zoom(imL, 1.0/args.shrink)
         imR = scipy.ndimage.zoom(imR, 1.0/args.shrink)
@@ -129,11 +116,22 @@ def trw_s_2d_stereo():
     print(imL.shape, imL.dtype, imL.min(), imL.max())
     H, W = imL.shape[:2]
 
-    if False and os.path.isfile(CACHE_PATH):
-        f = np.load(CACHE_PATH)
+    def compute_block_diff(L, R):
+        if WINDOW_SIZE == 1:
+            return np.abs(L - R).mean(axis=2)
+        offy = WINDOW_SIZE/2
+        offx = WINDOW_SIZE/2
+        u = np.zeros(L.shape[:2], np.float32)
+        for y in range(offy, H - offy):
+            for x in range(offx, W - offx):
+                u[y, x] = ((L[y - offy : y - offy + WINDOW_SIZE, x - offx : x - offx + WINDOW_SIZE] \
+                        - R[y - offy : y - offy + WINDOW_SIZE, x - offx : x - offx + WINDOW_SIZE]) ** 2).mean()
+        return u
+
+    CACHE = 'cache.npz'
+    if os.path.isfile(CACHE):
+        f = np.load(CACHE)
         unary = f['unary']
-        pairwise = f['pairwise']
-        print('loaded from cache')
     else:
         # data cost
         unary = np.zeros((H, W, N_DISP), np.float32)
@@ -141,26 +139,31 @@ def trw_s_2d_stereo():
             disp = int((idisp - BASE_DISP) * STEP_SIZE)
             print(disp)
             if disp > 0:
-                unary[:, :, idisp] = np.abs(imL - np.concatenate([
+                unary[:, :, idisp] = compute_block_diff(imL, np.concatenate([
                     imR[:, disp:, :],
-                    imR[:, [-1]*abs(disp), :]], axis=1)).mean(axis=2)
+                    imR[:, [-1]*abs(disp), :]], axis=1))
             elif disp < 0:
-                unary[:, :, idisp] = np.abs(imL - np.concatenate([
+                unary[:, :, idisp] = compute_block_diff(imL, np.concatenate([
                     imR[:, [0]*abs(disp), :],
-                    imR[:, :disp, :]], axis=1)).mean(axis=2)
+                    imR[:, :disp, :]], axis=1))
             else:
-                unary[:, :, idisp] = np.abs(imL - imR).mean(axis=2)
-            #plt.imshow(unary[:, :, idisp])
-            #plt.show()
-        # pairwise cost = potts
+                unary[:, :, idisp] = compute_block_diff(imL, imR)
+            if args.show_unary:
+                plt.imshow(unary[:, :, idisp])
+                plt.show()
+        np.savez(CACHE, unary=unary)
+
+    # pairwise cost
+    if args.pairwise == 'Potts':
         pairwise = 1.0 - np.eye(N_DISP, dtype=np.float32)
-        # pairwise cost = truncated linear
+    if args.pairwise == 'linear':
+        pairwise = np.eye(N_DISP, dtype=np.float32)
         for y in range(N_DISP):
             for x in range(N_DISP):
                 pairwise[y, x] = min(4, np.abs(y - x))
-        np.savez(CACHE_PATH, unary=unary, pairwise=pairwise)
-        print('saved to cache')
+    pairwise *= args.coef
 
+    # find MAP of the MRF
     if args.method == 'local':
         print('method: Local best')
         disp = solve_local(unary, pairwise)
@@ -175,6 +178,8 @@ def trw_s_2d_stereo():
 
     fig, ax = plt.subplots(1, 1)
     ax.imshow(disp)
+    fig.suptitle('method: {}'.format(args.method))
+    fig.savefig('img_{}.png'.format(args.method))
 
 def main():
     trw_s_2d_stereo()
